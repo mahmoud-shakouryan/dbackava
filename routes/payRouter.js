4c44dfa6ba18b621c0da9d437daf119c0e6fe49a
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2022-07-11 02:30:24 -0700

    commit

--- routes/payRouter.js
+++ routes/payRouter.js
@@ -27,10 +27,10 @@ payRouter.post('/',isAuth,  expressAsyncHandler((req, res)=>{
     const params = { 'order_id': `${req.body.videoId}`, 'amount': req.body.price, 'callback': 'https://www.avayejan.ir/api/pay/callback', 'desc':`${req.body.videoId}` };
     axios.post('https://api.idpay.ir/v1/payment', params, {headers:{ 'Content-Type': 'application/json',  'X-API-KEY': '8140f12b-92de-4dac-b720-9b2e8dd8b6ec'}})
     .then(response =>{
-        console.log('resposnse from zarinpal request>>>>>', response);
-        const newPayment = new Payment({ user: req.user, amount: req.body.price, resNumber: response.data.data.authority});
+        console.log('resposnse from idPay request>>>>>', response);
+        const newPayment = new Payment({ user: req.user, amount: req.body.price, paymentId: response.data.id, paymentLink: response.data.link});
         newPayment.save().then(result=>{
-            return res.redirect(`https://www.zarinpal.com/pg/StartPay/${response.data.data.authority}`)
+            return res.send({ link: response.data.link})
         })
         .catch(err=> console.log('new payment error'));
     })
