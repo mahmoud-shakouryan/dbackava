4fecc9eb484d18f229ace9eb7525c7ebd96c5f74
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2022-07-26 12:41:14 -0700

    commit

--- routes/payRouter.js
+++ routes/payRouter.js
@@ -11,7 +11,7 @@ const payRouter = express.Router();
 payRouter.post('/',isAuth,  expressAsyncHandler((req, res)=>{
     console.log('umad pay');
     const params = { 'order_id': `${req.body.videoId}`, 'amount': req.body.price, 'callback': 'https://www.avayejaan.ir/myvideos', 'desc':`${req.body.videoId}` };
-    axios.post('https://api.idpay.ir/v1/payment', params, {headers:{ 'Content-Type': 'application/json',  'X-API-KEY': '8140f12b-92de-4dac-b720-9b2e8dd8b6ec', 'X-SANDBOX': true}})
+    axios.post('https://api.idpay.ir/v1/payment', params, {headers:{ 'Content-Type': 'application/json',  'X-API-KEY': '8140f12b-92de-4dac-b720-9b2e8dd8b6ec'}})
     .then(response =>{
         console.log('resposnse = require( idPay request>>>>>', response);
         const newPayment = new Payment({ user: req.user, amount: req.body.price, paymentId: response.data.id, paymentLink: response.data.link});
