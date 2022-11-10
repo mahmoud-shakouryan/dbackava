e66c16129ce11d4e69998a21ebb433d09a4888a6
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2022-11-10 13:00:53 +0330

    commit

--- routes/payRouter.js
+++ routes/payRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const expressAsyncHandler = require('express-async-handler');
 const Payment = require('../models/payment.js');
-const  User = require('../models/user.js');
 const { isAuth } = require('../util.js')
 const axios = require( 'axios');
 
@@ -25,39 +24,47 @@ payRouter.post('/', isAuth,  expressAsyncHandler((req, res)=>{
 
 
 payRouter.post('/status', expressAsyncHandler(async (req, res)=>{
-    const { status, order_id ,payId } = req.body;
-    // if( status == null){
-    //     const user = await User.findById(userId)
-    //     return res.send( user.paidVidIds )
-    // }
-    if(status == 10){
-        return res.send({ message: 'پرداخت انجام نشده است'});
-    }
-    else if(status == 2){
-        return res.send({ message: 'پرداخت ناموفق بوده است'});
-    }
-    else if(status ==3){
-        return res.send({ message: 'خطا رخ داده است'});
-    }
-    else if(status == 1){
-        const payment = await Payment.findOne({ paymentId: payId });
-        if(!payment){
-            return res.send({ message: 'چنین تراکنشی وجود ندارد'})
-        }
-        const body = { 'id': payId, 'order_id': order_id }
-        try{
-            const response = await axios.post('https://api.idpay.ir/v1.1/payment/inquiry', body, { headers: { 'Content-Type': 'application/json', 'X-API-KEY': '3e1b9437-893a-417f-9355-1ba934862ccb'}})
-            if( response.status == 200){
-                console.log('response verify /status')
-                return res.send({ token: response.data.payer.name, mail: response.data.payer.mail})
+    const { status, order_id, payId } = req.body;
+    switch (+status) {
+        case 1: 
+            console.log('umad case 1');
+            const body = { 'id': payId, 'order_id': order_id }
+            try{
+                const response = await axios.post('https://api.idpay.ir/v1.1/payment/inquiry', body, { headers: { 'Content-Type': 'application/json', 'X-API-KEY': '3e1b9437-893a-417f-9355-1ba934862ccb'}})
+                if( response.status == 200){
+                    console.log('response verify /status')
+                    return res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message: 'پرداخت انجام نشده است'})
+                }
             }
-        }
-        catch(err){
-            console.log(err)
-        }
-
+            catch(err){
+                console.log(err)
+            }
+            break
+        case 2:
+            console.log('umad case 2');
+            return res.send({ message: 'پرداخت ناموفق بوده است'});
+        case 3:
+            console.log('umad case 3');
+            return res.send({ message: 'خطا رخ داده است'});
+        case 10:
+            const payment = await Payment.findOne({ paymentId: payId });
+            if(!payment){
+                return res.send({ message: 'چنین تراکنشی وجود ندارد'})
+            }
+            try{
+                if( +response.status == 200){
+                    const body = { 'id': payId, 'order_id': order_id }
+                    const response = await axios.post('https://api.idpay.ir/v1.1/payment/inquiry', body, { headers: { 'Content-Type': 'application/json', 'X-API-KEY': '3e1b9437-893a-417f-9355-1ba934862ccb'}})
+                    console.log('response verify /status')
+                    return res.send({ token: response.data.payer.name, mail: response.data.payer.mail})
+                }
+            }
+            catch(err){
+                console.log(err)
+            }
+        default:
+            break;
     }
-    
 }));
 
 
