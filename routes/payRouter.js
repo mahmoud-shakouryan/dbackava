2df11724247956b44aeb64e50ccc6d05f0844472
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2023-01-28 14:46:17 +0330

    commit

--- routes/payRouter.js
+++ routes/payRouter.js
@@ -27,41 +27,55 @@ payRouter.post('/status', expressAsyncHandler(async (req, res)=>{
     const { status, order_id, payId } = req.body;
     switch (+status) {
         case 1: 
-            console.log('umad case 1');
-            const body = { 'id': payId, 'order_id': order_id }
-            try{
-                const response = await axios.post('https://api.idpay.ir/v1.1/payment/inquiry', body, { headers: { 'Content-Type': 'application/json', 'X-API-KEY': '3e1b9437-893a-417f-9355-1ba934862ccb'}})
-                if( response.status == 200){
-                    console.log('response verify /status')
-                    return res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message: 'پرداخت انجام نشده است'})
-                }
-            }
-            catch(err){
-                console.log(err)
-            }
-            break
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message: 'پرداخت انجام نشده است'})
+            break;
         case 2:
-            console.log('umad case 2');
-            return res.send({ message: 'پرداخت ناموفق بوده است'});
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message: 'پرداخت ناموفق بوده است'});
+            break;
         case 3:
-            console.log('umad case 3');
-            return res.send({ message: 'خطا رخ داده است'});
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message: 'خطا رخ داده است'});
+            break;
+        case 4:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'بلوکه شده'});
+            break;
+        case 5:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'برگشت به پرداخت کننده'});
+            break;
+        case 6:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'برگشت خورده سیستمی'});
+            break;
+        case 7:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'انصراف از پرداخت'});
+            break;
+        case 8:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'به درگاه پرداخت منتقل شد'});
+            break;
+        case 100:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'پرداخت تایید شده است'});
+            break;
+        case 101:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'پرداخت قبلا تایید شده است'});
+            break;
+        case 200:
+            res.send({ token: response.data.payer.name, mail: response.data.payer.mail, message:'به دریافت کننده واریز شد'});
+            break;
         case 10:
-            const payment = await Payment.findOne({ paymentId: payId });
-            if(!payment){
-                return res.send({ message: 'چنین تراکنشی وجود ندارد'})
-            }
+            // const payment = await Payment.findOne({ paymentId: payId });
+            // if(!payment){
+            //     return res.send({ message: 'چنین تراکنشی وجود ندارد'})
+            // }
             try{
-                if( +response.status == 200){
-                    const body = { 'id': payId, 'order_id': order_id }
-                    const response = await axios.post('https://api.idpay.ir/v1.1/payment/inquiry', body, { headers: { 'Content-Type': 'application/json', 'X-API-KEY': '3e1b9437-893a-417f-9355-1ba934862ccb'}})
-                    console.log('response verify /status')
-                    return res.send({ token: response.data.payer.name, mail: response.data.payer.mail})
+                const body = { 'id': payId, 'order_id': order_id }
+                const verifyResponse = await axios.post('https://api.idpay.ir/v1.1/payment/verify', body, { headers: { 'Content-Type': 'application/json', 'X-API-KEY': '3e1b9437-893a-417f-9355-1ba934862ccb'}})
+                console.log('verifyResponse>>>>>>>',verifyResponse);
+                if( +verifyResponse.status == 200){
+                    return res.send({ token: verifyResponse.data.payer.name, mail: verifyResponse.data.payer.mail})
                 }
             }
             catch(err){
                 console.log(err)
             }
+            break;
         default:
             break;
     }
--- routes/videoRouter.js
+++ routes/videoRouter.js
@@ -40,10 +40,8 @@ myVidsLinksRouter.post('/uservids', expressAsyncHandler(async(req, res)=>{
 
 myVidsLinksRouter.post('/myvidslinks', expressAsyncHandler(async(req, res) =>{
     const fileNameInBucket = req.body.allFiles;
-    console.log('all files names',fileNameInBucket)
     const linksArr = [];
     for( let fileIndex in fileNameInBucket){
-        console.log(fileIndex)
         const s3 = new S3Client({ region: 'default', endpoint: 'https://s3.ir-thr-at1.arvanstorage.com', forcePathStyle: false, credentials: { accessKeyId: 'cd642d50-c891-4f1c-9d62-3c929e5b7e5c', secretAccessKey: '46c4b12ed300a4e49cfa8fc86d424c5f10137963feaf1655782750134996bbc9' }});
         const clientParams = { Bucket: 'avayejan', Key: fileNameInBucket[fileIndex].title };
         const signedRequest = new S3RequestPresigner(s3.config);
